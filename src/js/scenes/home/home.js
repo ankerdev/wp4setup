62d4a8619a10cc4d85d3486148e4580dad43730e
ankerdev <jonasankerjahr@gmail.com>
2018-05-03 16:51:02 +1000

    added react-hot-loader

--- src/js/scenes/home/home.js
+++ src/js/scenes/home/home.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { hot } from 'react-hot-loader';
 
 const Home = () => (
   <main className="main flex--down">
@@ -6,4 +7,4 @@ const Home = () => (
   </main>
 );
 
-export default Home;
+export default hot(module)(Home);
